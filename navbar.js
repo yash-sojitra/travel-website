3b0dc9f6092a1129e316e5248e8493686fc5f0a0
yashsojitra97 <yashsojitra97@gmail.com>
2023-04-30 23:34:32 +0530

    JS complete for navbar

--- navbar.js
+++ navbar.js
@@ -6,6 +6,7 @@ const text = document.querySelector(".title-text");
 const front = document.querySelector(".front img");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
+const burger = document.querySelector(".burger");
 
 window.addEventListener("mouseover", (e) => {
     console.log(e.target.classList);
@@ -37,3 +38,25 @@ const scene = new ScrollMagic.Scene({
     .setTween(parallaxTl)
     .addIndicators()
     .addTo(controller);
+
+
+
+burger.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("active")) {
+        e.target.classList.add("active");
+        gsap.to(".line1", 0.5, { rotate: "45", y: 6, background: "black" });
+        gsap.to(".line2", 0.5, { rotate: "-45", y: -6, background: "black" });
+        gsap.to(".logo-link", 1, { color: "black" });
+        gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+        gsap.to(".cursor", 1, { borderColor: "black" });
+        document.body.classList.add("hide");
+    } else {
+        e.target.classList.remove("active");
+        gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+        gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+        gsap.to(".logo-link", 1, { color: "white" });
+        gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+        gsap.to(".cursor", 1, { borderColor: "white" });
+        document.body.classList.remove("hide");
+      }
+})
